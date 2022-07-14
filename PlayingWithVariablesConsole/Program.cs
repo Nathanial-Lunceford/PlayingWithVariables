fc4a51c82f753de34e4c10683782fdb2e7dc9e23
Nate <nathanial.lunceford@gmail.com>
2022-07-14 14:05:18 -0500

    initial commit

---
+++ PlayingWithVariablesConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string thisString = "String";
+int thisInt = 30;
+char thisChar = 'S';
+bool thisBool = false;
+double thisDouble = 5.24;
+decimal thisDecimal = 2.34m;
+
+Console.WriteLine($"My dog's name is {thisString}, he is {thisInt} years old, " +
+    $"his initial is {thisChar}, we got him {thisDouble} weeks ago and " +
+    $"he has already peed on the bed {thisDecimal} times " +
+    $"Does he have ringworm? {thisBool}");
